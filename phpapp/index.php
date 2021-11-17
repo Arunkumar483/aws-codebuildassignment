b532ade44019d7e0a2656f6e486fe7540830fbd6
Arunkumar483 <gmarun2000@gmail.com>
2021-11-17 17:28:06 +0530

    test

--- phpapp/index.php
+++ phpapp/index.php
@@ -8,7 +8,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 
 <html>
 <head>
-    <title>Homepage</title>
+    <title>Homepage233</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.18.3/locale/bootstrap-table-el-GR.min.js" integrity="sha512-0lPAvFKuw7btN37VscT1sXWLbVV9+yif9p8BWRytDFQLmJXaSzizfBerc5klj75dvwdXWOD2yd6rLvCJKY16aQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
